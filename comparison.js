a56208d1739ff0128d96f3f4609a6326d552c774
unknown <nshienarocell@gmail.com>
2024-09-12 22:15:12 -0800

    Initial commit with comparison.js

---
+++ comparison.js
@@ -0,0 +1,34 @@
+let firstFavNumb = 20;
+let secondFavNumb = 25;
+//Greater than
+let greaterThan = firstFavNumb > secondFavNumb;
+console.log(greaterThan);
+//Less than
+let lessThan = firstFavNumb < secondFavNumb;
+console.log(lessThan);
+//Greater than or equal to
+let greaterThanOrEqualTo = firstFavNumb >= secondFavNumb;
+console.log(greaterThanOrEqualTo);
+//Less than oe equal to
+let lessThanOrEqualTo = firstFavNumb <= secondFavNumb;
+console.log(lessThanOrEqualTo);
+//Loose equality
+let looseEquality = firstFavNumb === secondFavNumb;
+console.log(looseEquality);
+//Strict inequality
+let strictInequality = firstFavNumb !== secondFavNumb;
+console.log(strictInequality);
+//Loose Inequality
+let looseInequality = firstFavNumb != secondFavNumb;
+console.log(looseInequality);
+//AND
+let and = firstFavNumb > secondFavNumb && secondFavNumb < firstFavNumb;
+console.log(and);
+//OR
+let or = firstFavNumb === secondFavNumb || secondFavNumb === firstFavNumb;
+console.log(or);
+//NOT
+let isPassed = true;
+let isFailed = false;
+console.log(!isPassed);
+console.log(!isFailed);
